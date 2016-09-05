191c57c901a0ef53a6a11b6bd7753f79c3976f33
Senkan1990 <h14491002@gmail.com>
2016-09-05 15:58:23 +0900

    Add files via upload

---
+++ _01.serialtest/_01.serialtest.ino
@@ -0,0 +1,30 @@
+/*Serial commute
+20160905
+by Hank Cheng
+*/
+void setup() {
+  Serial.begin(115200);
+  pinMode(ledPin , OUTPUT);
+}
+int neme = 100 ;
+void loop() {
+  
+  if(Serial.available() > 0){
+    char inChar =Serial.read();
+    if(inChar=='h'||inChar=='H'){
+      digitalWrite(ledPin,HIGH);
+      Serial.print(neme*2,DEC);
+      Serial.print("###");
+    
+     
+    }else if (inChar=='I'||inChar=='i'){
+          digitalWrite(ledPin,LOW);
+          Serial.print(neme,DEC);
+          Serial.print("!!!");
+          }
+          
+   }
+
+  
+}
+
